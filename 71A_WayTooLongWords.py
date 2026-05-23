a52ed4bc2bff0ea159713cda44ff9bf1217fd314
Bhargav Nagalamadaka <38589200+bharg647@users.noreply.github.com>
2026-05-22 20:27:57 -0400

    Finished 71A_WayTooLongWords.py

---
+++ 71A_WayTooLongWords.py
@@ -0,0 +1,15 @@
+
+def print_abbr(s: str) -> None:
+    if len(s) > 10:
+        print(s[0]+str(len(s))+s[len(s)-1])
+    else:
+        print(s)
+
+
+if __name__ == "__main__":
+    n = int(input())
+    
+    for _ in range(n):
+        s = input()
+        
+        print_abbr(s)
