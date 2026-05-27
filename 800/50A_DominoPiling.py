556bb5d4eaf05e4a2bf4143ee32c03ffc8687ba1
Bhargav Nagalamadaka <38589200+bharg647@users.noreply.github.com>
2026-05-26 20:50:47 -0400

    Finished 50A_DominoPiling.py
    Thought of a better solution.

--- 800/50A_DominoPiling.py
+++ 800/50A_DominoPiling.py
@@ -1,13 +1,7 @@
-
 def print_number_dominos(m: int, n: int) -> None:
     area = m*n
-    
-    if area % 2 == 0:
-        print(area//2)
-        return
-    else:
-        result = (area-1)//2
-        print(result)
+    print(area//2)
+        
 
 if __name__ == "__main__":
     [m,n] = [int(x) for x in input().split(" ")]
