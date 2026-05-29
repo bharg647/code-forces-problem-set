86a43c3f2a3d6fb4b4fe703fd2b3efacb42e54ba
Bhargav Nagalamadaka <38589200+bharg647@users.noreply.github.com>
2026-05-29 00:33:40 -0400

    Finished 263A_BeautifulMatrix.py

---
+++ 800/263A_BeautifulMatrix.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+    
+    one_index = None
+    matrix = []
+    for i in range(5):
+        matrix_row = input().split()
+        if "1" in matrix_row:
+            one_index = (i, matrix_row.index("1"))
+        matrix.append(matrix_row)
+    
+    target_index = (2,2)
+    
+    min_moves = abs(one_index[0]-target_index[0])+abs(one_index[1]-target_index[1])
+    print(min_moves)
