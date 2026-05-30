b21c3698cc7aa1468bef1a5fccb61fe9fd789bce
bharg647 <brgvnag1@gmail.com>
2026-05-29 20:28:56 -0400

    Finished 977A_WrongSubtraction.py

---
+++ 800/977A_WrongSubtraction.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__":
+    [n,k] = list(map(int, input().split()))
+
+    for _ in range(k):
+        if n % 10 != 0:
+            n -= 1
+        else:
+            n //= 10
+
+    print(n)
+
+
