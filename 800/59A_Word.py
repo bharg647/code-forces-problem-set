758af4a6efaa595633fad1a5daa5b64a63a44544
bharg647 <brgvnag1@gmail.com>
2026-05-29 20:23:44 -0400

    Finished 59A_Word.py

---
+++ 800/59A_Word.py
@@ -0,0 +1,16 @@
+if __name__ == "__main__":
+    s = input()
+
+    num_of_upper = 0
+    num_of_lower = 0
+    for c in s:
+        if c.upper() == c:
+            num_of_upper += 1
+        else:
+            num_of_lower += 1
+
+    if num_of_upper > num_of_lower:
+        print(s.upper())
+    else:
+        print(s.lower())
+
