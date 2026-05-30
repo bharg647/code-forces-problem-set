d2e5dc4f1a72ca6ceee8576a8bef09b52d866235
bharg647 <brgvnag1@gmail.com>
2026-05-30 10:48:18 -0400

    Worked on 677A_VanyaAndFence.py

--- 800/677A_VanyaAndFence.py
+++ 800/677A_VanyaAndFence.py
@@ -2,5 +2,35 @@ if __name__ == "__main__":
     [n,h] = list(map(int, input().split()))
     list_of_heights = list(map(int, input().split()))
 
+    current_amount = 0
+    width = 0
+    for height in list_of_heights:
+        current_amount += height
+        if current_amount < h:
+            continue
+        elif current_amount == h:
+            current_amount = 0
+            width += 1
+        else:
+            if (current_amount-height) != 0:
+                width += 1
+            
+            if height > h:
+                current_amount = 0
+                width += 2
+            elif height == h:
+                current_amount = 0
+                width += 1
+            else: 
+                current_amount = height
+
+    if current_amount > 0:
+        width += 1
+
+    print(width)
+
+
+
+
 
 
